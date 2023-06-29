8ca20028d484936c66b82acbebd7197787aa68b8
Suraj Pathak <surajpathak2057@gmail.com>
2023-06-29 23:13:17 +0545

    Add filtering

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import JobItem from "./JobItem";
 
 const App = () => {
   const [jobs, setJobs] = useState([]);
+  const [activeKeywords, setActiveKeywords] = useState([]);
 
   useEffect(() => {
     fetch(
@@ -21,11 +22,58 @@ const App = () => {
       });
   }, []);
 
+  const filteredJobs = jobs.filter((job) => {
+    return activeKeywords.every((keyword) => job.keywords.includes(keyword));
+  });
+
+  const addActiveKeyword = (keyword) => {
+    if (activeKeywords.includes(keyword))
+      setActiveKeywords(activeKeywords.filter((key) => key !== keyword));
+    else setActiveKeywords([...activeKeywords, keyword]);
+  };
+
+  const removeActiveKeyword = (keyword) => {
+    setActiveKeywords(activeKeywords.filter((kw) => kw !== keyword));
+  };
+
   if (jobs.length === 0) return <div>Loading...</div>;
   return (
-    <div>
-      {jobs.map((job, index) => (
-        <JobItem key={index} job={job} />
+    <div className="container">
+      {activeKeywords.length > 0 && (
+        <div className="active-keywords bg-white p-2 mt-3 d-flex justify-content-between align-items-center">
+          <div className="d-flex">
+            {activeKeywords.map((keyword) => (
+              <div key={keyword}>
+                <span className="active-keyword-btn me-2">
+                  <span className="active-keyword-btn-text ps-2 pe-1">
+                    {keyword}
+                  </span>
+                  <span
+                    className="del-btn fw-bold"
+                    onClick={() => removeActiveKeyword(keyword)}
+                  >
+                    &times;
+                  </span>
+                </span>
+              </div>
+            ))}
+          </div>
+          <button
+            className="btn btn-sm btn-secondary"
+            onClick={() => setActiveKeywords([])}
+          >
+            Clear
+          </button>
+        </div>
+      )}
+
+      {filteredJobs.map((job, index) => (
+        <JobItem
+          key={index}
+          job={job}
+          activeKeywords={activeKeywords}
+          addActiveKeyword={addActiveKeyword}
+        />
       ))}
     </div>
   );
--- src/JobItem.js
+++ src/JobItem.js
@@ -1,23 +1,40 @@
-const JobItem = ({ job }) => {
-  const postedTimeAgo = formatPostedDate(job.posted_on)
+const JobItem = ({ job, activeKeywords, addActiveKeyword }) => {
+  const postedTimeAgo = formatPostedDate(job.posted_on);
   return (
-    <div className="job-item container my-3 p-3">
-      <div className="d-flex align-items center">
-        <div className="company-logo">
-          <img
-            src={job.company_logo}
-            alt="Company Logo"
-            className="rounded-circle"
-            width={80}
-            height={80}
-          />
-        </div>
-        <div className="job-details mx-3">
-          <div className="text-success">{job.company}</div>
-          <div className="fw-bold">{job.position}</div>
-          <div className="text-muted">
-            {postedTimeAgo} - {job.timing} - {job.location}
+    <div className="job-item my-3 p-3 shadow rounded">
+      <div className="d-flex align-items-center justify-content-between">
+        <di className="d-flex align-items-center">
+          <div className="company-logo">
+            <img
+              src={job.company_logo}
+              alt="Company Logo"
+              className="rounded-circle"
+              width={80}
+              height={80}
+            />
           </div>
+          <div className="job-details mx-3">
+            <div className="text-success">{job.company}</div>
+            <div className="fw-bold">{job.position}</div>
+            <div className="text-muted">
+              {postedTimeAgo} - {job.timing} - {job.location}
+            </div>
+          </div>
+        </di>
+        <div className="keywords">
+          {job.keywords.map((keyword) => (
+            <button
+              key={keyword}
+              className={`btn ${
+                activeKeywords.includes(keyword)
+                  ? "btn-success"
+                  : "btn-outline-success"
+              } me-2 mb-1`}
+              onClick={() => addActiveKeyword(keyword)}
+            >
+              {keyword}
+            </button>
+          ))}
         </div>
       </div>
     </div>
@@ -27,23 +44,20 @@ const JobItem = ({ job }) => {
 export default JobItem;
 
 const formatPostedDate = (postedOn) => {
-    const currentTime = new Date().getTime();
-    // Calculate the difference between current timestamp and postedOn timestamp
-    const timeDiff = currentTime - postedOn;
-    
-    const millisecondsInHour = 60 * 60 * 1000;
-    const hours = Math.floor(timeDiff / millisecondsInHour)
-    if (hours < 24)
-        return `${hours} hour${hours > 1 ? 's' : ''} ago`
+  const currentTime = new Date().getTime();
+  // Calculate the difference between current timestamp and postedOn timestamp
+  const timeDiff = currentTime - postedOn;
 
-    const days = Math.floor(hours / 24)
-    if (days < 7)
-        return `${days} day${days > 1 ? 's' : ''} ago`
-    
-    const weeks = Math.floor(days / 7)
-    if (weeks < 4)
-        return `${weeks} week${weeks > 1 ? 's' : ''} ago`
+  const millisecondsInHour = 60 * 60 * 1000;
+  const hours = Math.floor(timeDiff / millisecondsInHour);
+  if (hours < 24) return `${hours} hour${hours > 1 ? "s" : ""} ago`;
 
-    const months = Math.floor(weeks / 4)
-        return `${months} month${months > 1 ? 's' : ''} ago`
-}
+  const days = Math.floor(hours / 24);
+  if (days < 7) return `${days} day${days > 1 ? "s" : ""} ago`;
+
+  const weeks = Math.floor(days / 7);
+  if (weeks < 4) return `${weeks} week${weeks > 1 ? "s" : ""} ago`;
+
+  const months = Math.floor(weeks / 4);
+  return `${months} month${months > 1 ? "s" : ""} ago`;
+};
